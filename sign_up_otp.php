3e12719403cbae3acbf7d890339eaab76e42b94a
shadiashafi <shadia.shafi@gmail.com>
2017-03-26 15:57:44 +0530

    mail fn

---
+++ sign_up_otp.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+	div{
+		height: 10px;
+		margin-left: 10px;
+		padding-top: 30px;
+		padding-left: 10px;
+	}
+	div input{
+		height: 20px;
+	}
+</style>
+	<title></title>
+</head>
+<body>
+<form method="post" action="signup_otp.php">
+
+<div>
+name:   <input type="text" name="name">
+</div>
+<div>
+Email:   <input type="email" name="email">
+</div>
+<div>
+password:<input type="password" name="password">
+</div>
+<div>
+Status:   <textarea name="status"></textarea></div>
+<div><button type="submit">signup</button></div>
+</form>
+</body>
+</html>
